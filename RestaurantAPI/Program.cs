fea22d6466759efba801215a449947240d13ad4a
Piotr Błaszczak <piotrblaszczakooo@gmail.com>
2023-10-15 18:10:14 +0200

    Added entity framework and configured

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -1,16 +1,24 @@
 using RestaurantAPI;
+using RestaurantAPI.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddTransient<IWeatherForcastService, WeatherForcastService>();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<RestaurantDbContext>();
+builder.Services.AddScoped<RestaurantSeeder>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<RestaurantSeeder>();
+
+seeder.Seed();
 
 app.UseHttpsRedirection(); // Zapytanie automatycznie przekierowywane na https
 
